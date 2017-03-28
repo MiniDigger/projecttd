333494197171f21cfb568426b7b75205e3a57ce5
MiniDigger <admin@minidigger.me>
2017-03-28 23:16:22 +0200

    Initial commit

---
+++ android/src/me/minidigger/projecttd/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package me.minidigger.projecttd;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import me.minidigger.projecttd.ProjectTD;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new ProjectTD(), config);
+	}
+}
---
+++ desktop/src/me/minidigger/projecttd/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package me.minidigger.projecttd.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import me.minidigger.projecttd.ProjectTD;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ProjectTD(), config);
+	}
+}
